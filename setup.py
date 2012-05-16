470cc7811b8c3a20dae8c5ff3674261f8afb8cfc
Bruno Panara <bruno.panara@gmail.com>
2012-05-16 17:24:11 +0100

    added manifest file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='Minidetector',
     version='0.1',
-    packages=['minidetecror',],
+    packages=['minidetector',],
     license='BSD License',
     long_description=open('README.txt').read(),
 )
