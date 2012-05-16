8cbd0bf3e8ebe757541051b56ab56036b5da4f3a
Bruno Panara <bruno.panara@gmail.com>
2012-05-16 17:12:52 +0100

    added readme.txt and setup.pu

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Minidetector',
+    version='0.1',
+    packages=['minidetecror',],
+    license='BSD License',
+    long_description=open('README.txt').read(),
+)
